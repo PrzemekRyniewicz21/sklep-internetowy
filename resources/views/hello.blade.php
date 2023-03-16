4511a0f7e869acd6045f9cb5ee87a8c34b2f8414
PrzemekRyniewicz21 <ryniewicz.przemyslaw98@gmail.com>
2023-03-16 20:48:09 +0100

    ini commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,1 @@
+<h3> HELLO BLADE </h3>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+// #8
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', [HelloController::class, 'show']);
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
